e55e547e18a77d158688c435a6938933ad5ff64f
Kevin Woo <kevinawoo@gmail.com>
2020-10-30 13:18:59 -0700

    reset to master

--- bin/download_images.go
+++ bin/download_images.go
@@ -1,165 +1,167 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
-	"unicode"
+  "fmt"
+  "github.com/jessevdk/go-flags"
+  "io"
+  "io/ioutil"
+  "net/http"
+  "net/url"
+  "os"
+  "path/filepath"
+  "regexp"
+  "strings"
+  "unicode"
 )
 
 type Opts struct {
-	Directory string `short:"d" long:"directory" description:"The absolute path of the dir to scan, try '-d $PWD'" required:"true"`
-	ImageDir  string `short:"i" long:"image-dir" description:"The absolute path of where to put images, try '-i $PWD/images/'" required:"true"`
-	CheckOnly bool   `long:"check" description:"Exits non zero on images not hosted by Armory or stored in the repo" required:"false"`
+  Directory string `short:"d" long:"directory" description:"The absolute path of the dir to scan, try '-d $PWD'" required:"true"`
+  ImageDir  string `short:"i" long:"image-dir" description:"The absolute path of where to put images, try '-i $PWD/images/'" required:"true"`
+  CheckOnly bool   `long:"check" description:"Exits non zero on images not hosted by Armory or stored in the repo" required:"false"`
 }
 
-func main() {
-	var opts Opts
-	_, err := flags.Parse(&opts)
-	if err != nil {
-		os.Exit(1)
-	}
 
-	switch {
-	case opts.CheckOnly:
-		imgUrls := imgsNotHostedByArmory(opts)
-		if len(imgUrls) > 0 {
-			fmt.Printf("Found %d images that are external to this this repo or Armory.\n\n", len(imgUrls))
-			for _, s := range imgUrls {
-				fmt.Println(s)
-			}
-			fmt.Println(`
+func main() {
+  var opts Opts
+  _, err := flags.Parse(&opts)
+  if err != nil {
+    os.Exit(1)
+  }
+
+  switch {
+  case opts.CheckOnly:
+    imgUrls := imgsNotHostedByArmory(opts)
+    if len(imgUrls) > 0 {
+      fmt.Printf("Found %d images that are external to this this repo or Armory.\n\n", len(imgUrls))
+      for _, s := range imgUrls {
+        fmt.Println(s)
+      }
+      fmt.Println(`
 You can download them by doing:
   docker run -v "$(pwd):/src" --workdir /src golang:1.12.0 go run -mod=vendor bin/download_images.go -d /src -i /src/images/`)
-			os.Exit(1)
-		}
-	default:
-		fmt.Println("Looking for images to download")
-		_ = filepath.Walk(opts.Directory, rewriteMarkdownWithSavedImages(opts))
-	}
+      os.Exit(1)
+    }
+  default:
+    fmt.Println("Looking for images to download")
+    _ = filepath.Walk(opts.Directory, rewriteMarkdownWithSavedImages(opts))
+  }
 
-	return
+  return
 }
 
 const markdownImageUrlRegex = `!\[.*]\((https?://.*)\)`
 
 func imgsNotHostedByArmory(opts Opts) []string {
-	var imageUrls []string
+  var imageUrls []string
 
-	_ = filepath.Walk(opts.Directory, func(filePath string, info os.FileInfo, err error) error {
-		if !shouldExploreFilePath(filePath) {
-			return nil
-		}
+  _ = filepath.Walk(opts.Directory, func(filePath string, info os.FileInfo, err error) error {
+    if !shouldExploreFilePath(filePath) {
+      return nil
+    }
 
-		fileContents, _ := ioutil.ReadFile(filePath)
+    fileContents, _ := ioutil.ReadFile(filePath)
 
-		markdownImageUrlMatch := regexp.MustCompile(markdownImageUrlRegex)
+    markdownImageUrlMatch := regexp.MustCompile(markdownImageUrlRegex)
 
-		if markdownImageUrlMatch.Match(fileContents) {
-			lines := strings.Split(string(fileContents), "\n")
+    if markdownImageUrlMatch.Match(fileContents) {
+      lines := strings.Split(string(fileContents), "\n")
 
-			for _, line := range lines {
-				if markdownImageUrlMatch.MatchString(line) {
-					imgs := markdownImageUrlMatch.FindStringSubmatch(line)
-					imageUrls = append(imageUrls, imgs[1:]...)
-				}
-			}
-		}
-		return nil
-	})
+      for _, line := range lines {
+        if markdownImageUrlMatch.MatchString(line) {
+          imgs := markdownImageUrlMatch.FindStringSubmatch(line)
+          imageUrls = append(imageUrls, imgs[1:]...)
+        }
+      }
+    }
+    return nil
+  })
 
-	return imageUrls
+  return imageUrls
 }
 
 func rewriteMarkdownWithSavedImages(opts Opts) filepath.WalkFunc {
-	return func(filePath string, info os.FileInfo, err error) error {
-		if !shouldExploreFilePath(filePath) {
-			return nil
-		}
+  return func(filePath string, info os.FileInfo, err error) error {
+    if !shouldExploreFilePath(filePath) {
+      return nil
+    }
 
-		fileContents, _ := ioutil.ReadFile(filePath)
+    fileContents, _ := ioutil.ReadFile(filePath)
 
-		markdownImageUrlMatch := regexp.MustCompile(markdownImageUrlRegex)
+    markdownImageUrlMatch := regexp.MustCompile(markdownImageUrlRegex)
 
-		if markdownImageUrlMatch.Match(fileContents) {
-			var newFileContents string
-			newFilePath := filePath + ".new"
-			newFile, _ := os.Create(newFilePath)
+    if markdownImageUrlMatch.Match(fileContents) {
+      var newFileContents string
+      newFilePath := filePath + ".new"
+      newFile, _ := os.Create(newFilePath)
 
-			lines := strings.Split(string(fileContents), "\n")
+      lines := strings.Split(string(fileContents), "\n")
 
-			newLine := ""
-			for _, line := range lines {
-				newLine = line
+      newLine := ""
+      for _, line := range lines {
+        newLine = line
 
-				if markdownImageUrlMatch.MatchString(line) {
-					urls := markdownImageUrlMatch.FindStringSubmatch(line)
-					for _, urlString := range urls[1:] {
-						localImgPath := downloadUrl(opts, urlString)
+        if markdownImageUrlMatch.MatchString(line) {
+          urls := markdownImageUrlMatch.FindStringSubmatch(line)
+          for _, urlString := range urls[1:] {
+            localImgPath := downloadUrl(opts, urlString)
 
-						if localImgPath != "" {
-							newLine = strings.Replace(newLine, urlString, localImgPath, -1)
-						}
-					}
-				}
+            if localImgPath != "" {
+              newLine = strings.Replace(newLine, urlString, localImgPath, -1)
+            }
+          }
+        }
 
-				newFileContents += newLine + "\n"
-			}
+        newFileContents += newLine + "\n"
+      }
 
-			// remove all trailing whitespace and add just \n at end of the file
-			_, _ = newFile.WriteString(strings.TrimRightFunc(newFileContents, unicode.IsSpace) + "\n")
-			_ = os.Rename(newFilePath, filePath)
-		}
-		return nil
-	}
+      // remove all trailing whitespace and add just \n at end of the file
+      _, _ = newFile.WriteString(strings.TrimRightFunc(newFileContents, unicode.IsSpace) + "\n")
+      _ = os.Rename(newFilePath, filePath)
+    }
+    return nil
+  }
 }
 
 func shouldExploreFilePath(filePath string) bool {
-	if strings.Contains(filePath, "vendor") {
-		return false
-	}
+  if strings.Contains(filePath, "vendor") {
+    return false
+  }
 
-	return strings.HasSuffix(filePath, ".md") || strings.HasSuffix(filePath, ".html")
+  return strings.HasSuffix(filePath, ".md") || strings.HasSuffix(filePath, ".html")
 }
 
 // downloadUrl returns relative imagePath
 func downloadUrl(opts Opts, urlString string) string {
-	u, err := url.Parse(urlString)
+  u, err := url.Parse(urlString)
 
-	imgUrlPath := strings.Split(u.Path, "/")
-	imgDir := strings.TrimSuffix(opts.ImageDir, "/") // remove training slash if it's there
+  imgUrlPath := strings.Split(u.Path, "/")
+  imgDir := strings.TrimSuffix(opts.ImageDir, "/") // remove training slash if it's there
 
-	imgName := imgUrlPath[len(imgUrlPath)-1]
-	absImgPath := imgDir + "/" + imgUrlPath[len(imgUrlPath)-1]
+  imgName := imgUrlPath[len(imgUrlPath)-1]
+  absImgPath := imgDir + "/" + imgUrlPath[len(imgUrlPath)-1]
 
-	i := strings.Split(strings.TrimSuffix(imgDir, "/"), "/")
-	relativeImgPath := i[len(i)-1] + "/" + imgName
+  i := strings.Split(strings.TrimSuffix(imgDir, "/"), "/")
+  relativeImgPath := i[len(i)-1] + "/" + imgName
 
-	stat, err := os.Stat(absImgPath)
-	if err == nil && stat != nil {
-		fmt.Printf("exits: %s   as   %s\n", urlString, absImgPath)
-		return relativeImgPath
-	}
+  stat, err := os.Stat(absImgPath)
+  if err == nil && stat != nil {
+    fmt.Printf("exits: %s   as   %s\n", urlString, absImgPath)
+    return relativeImgPath
+  }
 
-	fmt.Printf("downloading %s   ->   %s\n", urlString, absImgPath)
-	resp, err := http.Get(urlString)
-	if err != nil || resp.StatusCode > 200 {
-		fmt.Printf("ERROR! %s\n", err.Error())
-		return ""
-	}
+  fmt.Printf("downloading %s   ->   %s\n", urlString, absImgPath)
+  resp, err := http.Get(urlString)
+  if err != nil || resp.StatusCode > 200 {
+    fmt.Printf("ERROR! %s\n", err.Error())
+    return ""
+  }
 
-	defer func() { _ = resp.Body.Close() }()
-	f, _ := os.Create(absImgPath)
-	defer func() { _ = f.Close() }()
-	_, _ = io.Copy(f, resp.Body)
+  defer func() { _ = resp.Body.Close() }()
+  f, _ := os.Create(absImgPath)
+  defer func() { _ = f.Close() }()
+  _, _ = io.Copy(f, resp.Body)
 
-	fmt.Printf("SAVED!\n")
+  fmt.Printf("SAVED!\n")
 
-	return relativeImgPath
+  return relativeImgPath
 }
--- utils/cmd/rename_images.go
+++ utils/cmd/rename_images.go
@@ -1,143 +1,143 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
-
-	log "github.com/sirupsen/logrus"
+  "github.com/jessevdk/go-flags"
+  log "github.com/sirupsen/logrus"
+  "io/ioutil"
+  "os"
+  "path/filepath"
+  "regexp"
+  "strings"
 )
 
 type Opts struct {
-	Directory string   `short:"d" long:"directory" description:"The absolute path of the dir to scan, try '-d $PWD'" required:"true"`
-	ImageDir  string   `short:"i" long:"image-dir" description:"The absolute path of where to put images, try '-i $PWD/images/'" required:"true"`
-	Excludes  []string `short:"e" long:"exclude" description:"folder or regex to exclude when walking the 'directory'" required:"false"`
+  Directory string   `short:"d" long:"directory" description:"The absolute path of the dir to scan, try '-d $PWD'" required:"true"`
+  ImageDir  string   `short:"i" long:"image-dir" description:"The absolute path of where to put images, try '-i $PWD/images/'" required:"true"`
+  Excludes  []string `short:"e" long:"exclude" description:"folder or regex to exclude when walking the 'directory'" required:"false"`
 }
 
 func main() {
-	var opts Opts
-	_, err := flags.Parse(&opts)
-	if err != nil {
-		os.Exit(1)
-	}
+  var opts Opts
+  _, err := flags.Parse(&opts)
+  if err != nil {
+    os.Exit(1)
+  }
 
-	imgPaths := getImagePaths(err, opts)
-	renamed := getNewImageNames(imgPaths)
-	err = renameImage(renamed)
-	if err != nil {
-		log.Fatal(err)
-	}
+  imgPaths := getImagePaths(err, opts)
+  renamed := getNewImageNames(imgPaths)
+  err = renameImage(renamed)
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	err = replaceContents(err, opts, renamed)
-	if err != nil {
-		log.Error("")
-		log.Fatal(err)
-	}
+  err = replaceContents(err, opts, renamed)
+  if err != nil {
+    log.Error("")
+    log.Fatal(err)
+  }
 }
 
 func replaceContents(err error, opts Opts, renamed []imageRename) error {
-	var excludedRegexes []*regexp.Regexp
-	for _, e := range opts.Excludes {
-		r, err := regexp.Compile(e)
-		if err != nil {
-			return err
-		}
+  var excludedRegexes []*regexp.Regexp
+  for _, e := range opts.Excludes {
+    r, err := regexp.Compile(e)
+    if err != nil {
+      return err
+    }
 
-		excludedRegexes = append(excludedRegexes, r)
-	}
+    excludedRegexes = append(excludedRegexes, r)
+  }
 
-	err = filepath.Walk(opts.Directory, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
+  err = filepath.Walk(opts.Directory, func(path string, info os.FileInfo, err error) error {
+    if info.IsDir() {
+      return nil
+    }
 
-		if filepath.Ext(path) != ".md" {
-			return nil
-		}
+    if filepath.Ext(path) != ".md" {
+      return nil
+    }
 
-		for _, e := range excludedRegexes {
-			if e.Match([]byte(path)) {
-				return nil
-			}
-		}
+    for _, e := range excludedRegexes {
+      if e.Match([]byte(path)) {
+        return nil
+      }
+    }
 
-		fileContents, _ := ioutil.ReadFile(path)
-		permissions, err := os.Stat(path)
-		if err != nil {
-			return err
-		}
+    fileContents, _ := ioutil.ReadFile(path)
+    permissions, err := os.Stat(path)
+    if err != nil {
+      return err
+    }
 
-		c := string(fileContents)
-		for _, r := range renamed {
-			c = strings.ReplaceAll(c, r.OldFileName, r.NewFileName)
-		}
+    c := string(fileContents)
+    for _, r := range renamed {
+      c = strings.ReplaceAll(c, r.OldFileName, r.NewFileName)
+    }
 
-		if string(fileContents) == c {
-			log.Infof("skipping: %s", path)
-			return nil
-		}
+    if string(fileContents) == c {
+      log.Infof("skipping: %s", path)
+      return nil
+    }
 
-		log.Infof("rewriting: %s", path)
-		err = ioutil.WriteFile(path, []byte(c), permissions.Mode())
-		if err != nil {
-			log.Error("cannot rename file")
-			return err
-		}
+    log.Infof("rewriting: %s", path)
+    err = ioutil.WriteFile(path, []byte(c), permissions.Mode())
+    if err != nil {
+      log.Error("cannot rename file")
+      return err
+    }
 
-		return nil
-	})
-	return err
+    return nil
+  })
+  return err
 }
 
 func renameImage(renamed []imageRename) error {
-	for _, r := range renamed {
-		if r.NewFileName == r.OldFileName {
-			continue
-		}
+  for _, r := range renamed {
+    if r.NewFileName == r.OldFileName {
+      continue
+    }
 
-		err := os.Rename(r.Folder+string(os.PathSeparator)+r.OldFileName, r.Folder+string(os.PathSeparator)+r.NewFileName)
-		if err != nil {
-			log.Errorf("could not rename image from\n'%s'\nto\n'%s'", r.OldFileName, r.NewFileName)
-			return err
-		}
-	}
-	return nil
+    err := os.Rename(r.Folder+string(os.PathSeparator)+r.OldFileName, r.Folder+string(os.PathSeparator)+r.NewFileName)
+    if err != nil {
+      log.Errorf("could not rename image from\n'%s'\nto\n'%s'", r.OldFileName, r.NewFileName)
+      return err
+    }
+  }
+  return nil
 }
 
 func getImagePaths(err error, opts Opts) []string {
-	imgPaths := []string{}
-	err = filepath.Walk(opts.ImageDir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
+  imgPaths := []string{}
+  err = filepath.Walk(opts.ImageDir, func(path string, info os.FileInfo, err error) error {
+    if info.IsDir() {
+      return nil
+    }
 
-		imgPaths = append(imgPaths, path)
-		return nil
-	})
-	if err != nil {
-		log.Fatalf("unable to get a list of images, %s", err)
-	}
-	return imgPaths
+    imgPaths = append(imgPaths, path)
+    return nil
+  })
+  if err != nil {
+    log.Fatalf("unable to get a list of images, %s", err)
+  }
+  return imgPaths
 }
 
 func getNewImageNames(imgPaths []string) []imageRename {
-	var renamed []imageRename
-	for _, path := range imgPaths {
-		img := imageRename{
-			OldFileName: filepath.Base(path),
-			NewFileName: strings.ReplaceAll(filepath.Base(path), " ", "-"),
-			Folder:      filepath.Dir(path),
-		}
+  var renamed []imageRename
+  for _, path := range imgPaths {
+    img := imageRename{
+      OldFileName: filepath.Base(path),
+      NewFileName: strings.ReplaceAll(filepath.Base(path), " ", "-"),
+      Folder:      filepath.Dir(path),
+    }
 
-		renamed = append(renamed, img)
-	}
-	return renamed
+    renamed = append(renamed, img)
+  }
+  return renamed
 }
 
 type imageRename struct {
-	Folder      string
-	OldFileName string
-	NewFileName string
+  Folder      string
+  OldFileName string
+  NewFileName string
 }
