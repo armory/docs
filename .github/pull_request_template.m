3dce2e6144a3f93f363134b1c1acb690bbd2fa3d
brian-armory <53799971+brian-armory@users.noreply.github.com>
2021-02-03 12:33:06 -0800

    Update pull_request_template.m

--- .github/pull_request_template.m
+++ .github/pull_request_template.m
@@ -1,4 +1,5 @@
-<!--Include the Jira or GitHub issue associated with this work if there is one -->
+<!-- Make sure your PR title matches the following format: <type>(<scope>): <subject> -->
+<!-- Include the Jira or GitHub issue associated with this work if there is one -->
 Resolves issue: 
 
 <!-- Share artifacts of the changes if they'd help your reviewer, e.g. a screenshot -->
