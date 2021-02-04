ca29ef3b6e649ab97e8e120941520f6d67acf2bd
aimeeu <aimeeu.opensource@gmail.com>
2021-02-04 15:30:11 -0600

    docs(pr-template): add line (#432)
    add line on how to format the jira ticket so the Jira-GH integration recognizes it

--- .github/pull_request_template.m
+++ .github/pull_request_template.m
@@ -1,5 +1,6 @@
 <!-- Make sure your PR title matches the following format: <type>(<scope>): <subject> -->
 <!-- Include the Jira or GitHub issue associated with this work if there is one -->
+<!-- If Jira, enclose the ticket in brackets example: [DOC-123] -->
 Resolves issue: 
 
 <!-- Share artifacts of the changes if they'd help your reviewer, e.g. a screenshot -->
