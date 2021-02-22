b43dc17e9055c5ffdc066203ba88d9c2a723ac7e
aimeeu <aimeeu.opensource@gmail.com>
2021-02-22 12:39:09 -0600

    tweak

--- .github/pull_request_template.m
+++ .github/pull_request_template.m
@@ -1,7 +1,7 @@
 <!-- Make sure your PR title matches the following format: <type>(<scope>): <subject> -->
 <!-- Include the Jira or GitHub issue associated with this work if there is one -->
 Resolves GitHub issue: <link to GH issue>
-<!-- If Jira, enclose the ticket in brackets. This format is required for the Jira-GitHub integration-->
+<!-- If Jira, enclose the ticket in brackets. Jira-GitHub integration requires this format. -->
 Resolves Jira: [JIRAPROJECT-123]
 
 <!-- Share artifacts of the changes if they'd help your reviewer, e.g. a screenshot -->
